e0c7bc595338c00774ed4c830caa4c5f41048cb6
sh1n0b1n0m0n0 <nindzja.t001@gmail.com>
2024-08-27 06:24:21 -0400

    edited train.py, general gitignore

--- train.py
+++ train.py
@@ -40,6 +40,14 @@ def main(
     train_config = yaml.safe_load(open(train_params_dir))["train_params"]
     num_classes = len(classes)
 
+    trainer = Trainer(experiment_name=train_config["experiment_name"], 
+                      ckpt_root_dir=CHECKPOINT_DIR)
+    
+    model_s = models.get(train_config["model"],
+                        num_classes=num_classes,
+                        pretrained_weights=train_config["pretrained_weights"]
+                        )
+
     train_params = {
         "silent_mode": train_config["silent_mode"],
         "average_best_models": train_config["average_best_models"],
@@ -48,7 +56,7 @@ def main(
         "lr_warmup_epochs": train_config["lr_warmup_epochs"],
         "initial_lr": train_config["initial_lr"],
         "lr_mode": train_config["lr_mode"],
-        "cosine_final_lr_ratio": train_config["warmup_mode"],
+        "cosine_final_lr_ratio": train_config["cosine_final_lr_ratio"],
         "optimizer": train_config["optimizer"],
         "optimizer_params": train_config["optimizer_params"],
         "zero_weight_decay_on_bias_and_bn": train_config["zero_weight_decay_on_bias_and_bn"],
@@ -112,14 +120,6 @@ def main(
     )
 
     train_data.dataset.transforms  # transforms are common image transformations
-
-    trainer = Trainer(experiment_name=train_config["experiment_name"], 
-                      ckpt_root_dir=CHECKPOINT_DIR)
-    
-    model_s = models.get(train_config["model"],
-                        num_classes=num_classes,
-                        pretrained_weights=train_config["pretrained_weights"]
-                        )
 
     trainer.train(model=model_s, 
                   training_params=train_params,
